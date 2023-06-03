04f98c33638f3b0d86df455e78b0d281bb5620cd
Rolem Galvez <mogalvez@pjlhuillier.com>
2023-06-03 14:08:09 +0800

    Add project files.

---
+++ HomeworkTwentyEight.ConsoleUI/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HomeworkTwentyEight.ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ HomeworkTwentyEight.Library/Models/EmployeeModel.cs
@@ -0,0 +1,36 @@
+﻿namespace HomeworkTwentyEight.Library.Models
+{
+    public class EmployeeModel
+    {
+        public string Name { get; set; }
+        public string Job { get; set; }
+        public decimal Salary { get; set; }
+
+        public EmployeeModel()
+        {
+            SetData("Juan Dela Cruz", "Software Engineer", 12345);
+        }
+
+        public EmployeeModel(string name)
+        {
+            SetData(name, "Software Engineer", 12345);
+        }
+
+        public EmployeeModel(string name, string job)
+        {
+            SetData(name, job, 12345);
+        }
+
+        public EmployeeModel(string name, string job, decimal salary)
+        {
+            SetData(name, job, salary);
+        }
+
+        public void SetData(string name, string job, decimal salary)
+        {
+            Name = name;
+            Job = job;
+            Salary = salary;
+        }
+    }
+}
